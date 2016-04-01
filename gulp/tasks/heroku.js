dd2f5946a20be913f520021254351d84c141d27b
Ryan Baker <ryan.da.baker@gmail.com>
2016-04-01 17:24:49 -0400

    not a good solution, but I think this will work

--- gulp/tasks/heroku.js
+++ gulp/tasks/heroku.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
-var serve = require('gulp-serve');
+var serve = require('gulp-connect');
 
 
 gulp.task('heroku:production', function() {
   serve({
     root: ['static/shared', 'static/style-guide'],
-    port: process.env.PORT || 9090
+    port: process.env.PORT || 9090,
+    fallback: 'static/style-guide/index.html'
+    livereload: false
   });
 });
