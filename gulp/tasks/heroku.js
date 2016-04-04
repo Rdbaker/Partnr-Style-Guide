6f7e50baa174bb653f4681304eb20a633fafd6df
Ryan Baker <ryan.da.baker@gmail.com>
2016-04-03 22:34:50 -0400

    Changes some things for heroku.

--- gulp/tasks/heroku.js
+++ gulp/tasks/heroku.js
@@ -3,6 +3,6 @@ var connect = require('gulp-connect');
 var config = require('../config').server;
 
 
-gulp.task('heroku:production', function() {
+gulp.task('heroku:production', ['build'], function() {
   connect.server(config);
 });
